6e4f743cacca3de28134872ea861b9efa21de951
Emiliza4444 <kapitoshka197611@gmail.com>
2021-04-16 22:51:16 +0300

    puzzles game
    Гра для конкурсу справжніх програмістів(Трохимчук Є.С., КН-18002Б)

---
+++ src/Main.java
@@ -0,0 +1,201 @@
+import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import static javax.swing.JOptionPane.showMessageDialog;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+public class Main extends Application {
+
+    private static Stage mainStage;
+    private static VBox mainLayout = new VBox();
+    private static Image image = new Image("dolphin.png");
+
+    static int numTiles = 9;
+    static Tile[][] tiles;
+
+    public static void main(String[] args){
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        mainStage = stage;
+
+        refresh();
+
+        Scene mainScene = new Scene(mainLayout);
+
+        mainStage.setScene(mainScene);
+        mainStage.setResizable(true);
+        mainStage.setTitle("Puzzle Slider");
+        mainStage.show();
+    }
+
+    private static void refresh(){
+        tiles = new Tile[(int) Math.sqrt(numTiles)][(int) Math.sqrt(numTiles)];
+        mainLayout.getChildren().clear();
+        mainLayout.getChildren().addAll(createMenu(), createGrid());
+
+        // Mix it up
+        Random rand = new Random();
+        for(int i = 0; i < 10000; i++){
+            int x = rand.nextInt((int) Math.sqrt(numTiles));
+            int y = rand.nextInt((int) Math.sqrt(numTiles));
+            tiles[x][y].clicked(true);
+        }
+    }
+
+    private static Parent createGrid() {
+        for(int i = 0; i < (int) Math.sqrt(numTiles); i++){
+            for(int j = 0; j < (int) Math.sqrt(numTiles); j++){
+                PixelReader reader = image.getPixelReader();
+                int size = 480/(int) Math.sqrt(numTiles);
+                WritableImage newImage = new WritableImage(reader, i*size,j*size,size,size);
+
+                Tile tile;
+
+                if(i == (int) Math.sqrt(numTiles)-1 && j == (int) Math.sqrt(numTiles)-1) {
+                    tile = new Tile(newImage, true,i,j);
+                }
+                else{
+                    tile = new Tile(newImage, false,i,j);
+                }
+                tiles[i][j] = tile;
+            }
+        }
+
+        GridPane gridLayout = new GridPane();
+        gridLayout.setHgap(1);
+        gridLayout.setVgap(1);
+
+        for(int i = 0; i < (int) Math.sqrt(numTiles); i++){
+            for(int j = 0; j < (int) Math.sqrt(numTiles); j++) {
+                gridLayout.add(tiles[i][j],i,j);
+            }
+        }
+        return gridLayout;
+    }
+
+    private static Node createMenu(){
+
+        MenuBar menuBar = new MenuBar();
+
+        Menu fileMenu = new Menu("Menu");
+
+        MenuItem aboutMenuItem = new MenuItem("Rules");
+        aboutMenuItem.setOnAction(event -> {
+            showMessageDialog(null,
+                    "Rules of the game\n" +
+                    "\n" +
+                    "The pieces cannot be lifted out of the frame or rearranged in any way, except by sliding them into an empty space by clicking mouse left button.\n" +
+                    "\n" +
+                    "The goal of a sliding puzzle is to arrange the pieces in a particular image. You can choose one of the in-game images or upload your one with 480x480 resolution.\n"+
+                    "\n" +
+                    "Good luck and have fun!");
+        });
+
+        MenuItem exitMenuItem = new MenuItem("Exit");
+        exitMenuItem.setOnAction(event -> mainStage.close());
+
+        fileMenu.getItems().addAll(aboutMenuItem,exitMenuItem);
+
+        Menu imageMenu = new Menu("Image");
+
+        MenuItem parrotImageMenuItem = new MenuItem("Parrot");
+        parrotImageMenuItem.setOnAction(actionEvent -> {image = new Image("parrot.png"); refresh();});
+        MenuItem dolphinImageMenuItem = new MenuItem("Dolphin");
+        dolphinImageMenuItem.setOnAction(actionEvent -> {image = new Image("dolphin.png"); refresh();});
+        MenuItem jellyBeanImageMenuItem = new MenuItem("Jelly Beans");
+        jellyBeanImageMenuItem.setOnAction(actionEvent -> {image = new Image("jellyBeans.png"); refresh();});
+        MenuItem customImageMenuItem = new MenuItem("Custom...");
+        customImageMenuItem.setOnAction(actionEvent -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.getExtensionFilters().addAll(
+                    new FileChooser.ExtensionFilter("Images", "*.gif", "*.jpg", "*.jpeg", "*.bmp", "*.png")
+            );
+            File selectedFile = fileChooser.showOpenDialog(mainStage);
+            try {
+                BufferedImage bufferedImage = ImageIO.read(selectedFile);
+                image = SwingFXUtils.toFXImage(bufferedImage, null);
+                refresh();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        imageMenu.getItems().addAll(dolphinImageMenuItem, parrotImageMenuItem, jellyBeanImageMenuItem, customImageMenuItem);
+
+        Menu difficultyMenu = new Menu("Difficulty");
+
+        MenuItem easyMenuItem = new MenuItem("Easy (3x3)");
+        easyMenuItem.setOnAction(actionEvent -> { numTiles = 9; refresh(); });
+        MenuItem mediumMenuItem = new MenuItem("Medium (4x4)");
+        mediumMenuItem.setOnAction(actionEvent -> { numTiles = 16; refresh(); });
+        MenuItem hardMenuItem = new MenuItem("Hard (5x5)");
+        hardMenuItem.setOnAction(actionEvent -> { numTiles = 25; refresh(); });
+        MenuItem insaneMenuItem = new MenuItem("Insane (8x8)");
+        insaneMenuItem.setOnAction(actionEvent -> { numTiles = 64; refresh(); });
+
+        difficultyMenu.getItems().addAll(easyMenuItem,mediumMenuItem,hardMenuItem,insaneMenuItem);
+
+        Menu hintMenu = new Menu("Hint");
+
+        ImageView hintImage = new ImageView(image);
+        hintImage.setPreserveRatio(true);
+        hintImage.setFitWidth(175);
+
+        MenuItem hintImageMenuItem = new MenuItem();
+
+        hintImageMenuItem.setGraphic(hintImage);
+
+        hintMenu.getItems().add(hintImageMenuItem);
+
+        menuBar.getMenus().addAll(fileMenu, imageMenu, difficultyMenu, hintMenu);
+
+        return menuBar;
+    }
+
+    static void checkWin() {
+        int counter = 0;
+        for(int i = 0; i < Main.tiles.length; i++){
+            for(int j = 0; j < Main.tiles[i].length; j++){
+                if(tiles[i][j].isOriginalBackground()){
+                    counter++;
+                }
+            }
+        }
+        if(counter == Main.numTiles){
+            won();
+        }
+    }
+
+    private static void won(){
+        Alert win = new Alert(Alert.AlertType.INFORMATION);
+        win.setTitle("Win!");
+        win.setHeaderText("Congratulations!");
+        win.setContentText("You solved the puzzle. Why not try a harder picture or more tiles?");
+        win.showAndWait();
+        for(Node node : mainLayout.getChildren()){
+            if(node instanceof GridPane) {
+                node.setDisable(true);
+            }
+        }
+    }
+}
---
+++ src/Tile.java
@@ -0,0 +1,79 @@
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import java.util.ArrayList;
+
+public class Tile extends Pane {
+
+    private Background originalBackground;
+    private Boolean isBlank = false;
+    private int x, y;
+
+    Tile(Image image, Boolean isBlank, int x, int y){
+
+        this.x = x;
+        this.y = y;
+        this.setMinHeight(480/(int) Math.sqrt(Main.numTiles));
+        this.setMinWidth(480/(int) Math.sqrt(Main.numTiles));
+        this.isBlank = isBlank;
+
+        if(!isBlank){
+            BackgroundImage backImage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+            originalBackground = new Background(backImage);
+            this.setBackground(new Background(backImage));
+        }
+        else{
+            BackgroundFill backgroundFill = new BackgroundFill(Color.WHITE,
+                    CornerRadii.EMPTY, Insets.EMPTY);
+            originalBackground = new Background(backgroundFill);
+        }
+
+        this.setOnMouseClicked(event -> clicked(false));
+    }
+
+    void clicked(Boolean ai){
+        if(!isBlank) {
+            int[] neighboursLocs = new int[] { -1, 0, 0, -1, 0, 1, 1, 0};
+
+            ArrayList<Tile> neighbours = new ArrayList<Tile>();
+
+            for (int i = 0; i < neighboursLocs.length; i++) {
+                int dx = neighboursLocs[i];
+                int dy = neighboursLocs[++i];
+
+                int newX = x + dx;
+                int newY = y + dy;
+
+                if (newX >= 0 && newX < (int) Math.sqrt(Main.numTiles) && newY >= 0 && newY < (int) Math.sqrt(Main.numTiles)) {
+                    neighbours.add(Main.tiles[newX][newY]);
+                }
+            }
+
+            for(Tile tile : neighbours){
+                if(tile.isBlank){
+                    Background temp = this.getBackground();
+                    tile.isBlank = false;
+                    tile.setBackground(temp);
+                    BackgroundFill backgroundFill = new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY);
+                    Background background = new Background(backgroundFill);
+                    this.setBackground(background);
+                    this.isBlank = true;
+                }
+            }
+        }
+        if(!ai) {
+            Main.checkWin();
+        }
+    }
+
+
+    public Node getStyleableNode() {
+        return null;
+    }
+
+    public boolean isOriginalBackground() {
+        return originalBackground.equals(this.getBackground());
+    }
+}
